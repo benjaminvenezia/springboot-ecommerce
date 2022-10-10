35dd95dfb14c5cd8e676cb6697c07cb723a8901a
Benjamin <Benjaminvenezia@hotmail.com>
2022-10-10 20:32:43 +0200

    add auth controller.

--- src/main/java/dev/benven/ecommerce/ECommerceApplication.java
+++ src/main/java/dev/benven/ecommerce/ECommerceApplication.java
@@ -1,8 +1,11 @@
 package dev.benven.ecommerce;
 
+import dev.benven.ecommerce.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
 public class ECommerceApplication {
 
